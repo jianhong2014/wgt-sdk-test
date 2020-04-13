19c819a144d4cf1c61e4ff06c8a8fa58818be6b1
jianhong2014 <jianhong2008@gmail.com>
2020-04-13 18:13:33 +0800

    升级sdk1.2

--- src/test/java/com/gvr/datahub/sdk/wgt/TestWgtDeviceSdk.java
+++ src/test/java/com/gvr/datahub/sdk/wgt/TestWgtDeviceSdk.java
@@ -4,10 +4,13 @@ package com.gvr.datahub.sdk.wgt;
 import com.alibaba.fastjson.JSON;
 import com.gvr.datahub.sdk.wgt.vo.NozzState;
 import com.gvr.datahub.sdk.wgt.vo.WgtKeepLive;
+import com.gvr.datahub.sdk.wgt.vo.WgtSetupRequest;
 import com.gvr.sdk.test.TestWgtSdkApplication;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,22 +22,75 @@ import java.util.function.Consumer;
 
 @SpringBootTest(classes = TestWgtSdkApplication.class)
 @RunWith(SpringRunner.class)
+@FixMethodOrder(MethodSorters.JVM)
 public class TestWgtDeviceSdk {
     private final  static Logger logger = LoggerFactory.getLogger(TestWgtDeviceSdk.class);
 
     @Autowired
     private WgtDeviceBuilder wgtDeviceBuilder;
 
+    private final String wgtId = "110";
+
+    private final String wgtIp = "10.28.188.31";
+
+    private final String wgtPort = "5000";
+
+    private final String wgtAddr = "32";
+
+    private final String dc = "ec2-52-82-4-65.cn-northwest-1.compute.amazonaws.com.cn";
+
+
     @Test
-    public void testRpc() throws InterruptedException {
-        logger.info("test hongjn2 ");
-        try(WgtDevice wgt = wgtDeviceBuilder.buildWgtDevice("localhost","110")){
-            NozzState nozzState = wgt.getNozzState("01");
+    public void testGetNozzleStat1()  {
+        logger.info("test testGetNozzleStat");
+        try(WgtDevice wgt = wgtDeviceBuilder.buildWgtDevice("localhost")){
+            NozzState nozzState = wgt.getNozzState(wgtId,1);
             logger.info("get nozzstate {}", JSON.toJSON(nozzState));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void testGetNozzleStat2()  {
+        logger.info("test testGetNozzleStat");
+        try(WgtDevice wgt = wgtDeviceBuilder.buildWgtDevice("localhost")){
+            NozzState nozzState = wgt.getNozzState(wgtId,1);
+            logger.info("get nozzstate {}", JSON.toJSON(nozzState));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void setBridge(){
+        logger.info("test setup bridge");
+        try(WgtDevice wgt = wgtDeviceBuilder.buildWgtDevice("localhost")){
+            //String rc = wgt.setupBridge(5000);
+          //  logger.info("set up  bridge {}", rc);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void setupWgt(){
+        logger.info("test setup wgt");
+        try(WgtDevice wgt = wgtDeviceBuilder.buildWgtDevice("localhost")){
+            WgtSetupRequest request = new WgtSetupRequest();
+            request.setTimeout(2000);
+            request.setWgtId(wgtId);
+            request.setWgtIp(wgtIp);
+            request.setWgtPort(wgtPort);
+            request.setTimeout(2000);
+            request.setWgtAddr(wgtAddr);
+            String  rc =  wgt.setupWgt(request);
+            logger.info("set up  wgt {}", rc);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
     @Test
     public void testDataListener(){
@@ -45,8 +101,8 @@ public class TestWgtDeviceSdk {
         Consumer<NozzState> visListener = (stat) -> {
             logger.info("visListener 识别到车环 {}",stat);
         };
-        try(WgtDevice wgt = wgtDeviceBuilder.buildWgtDevice("localhost","110")){
-           // wgt.addWgtHbConsumer(nozzStateProcessor);
+        try(WgtDevice wgt = wgtDeviceBuilder.buildWgtDevice("161.189.102.2")){
+            wgt.addWgtHbConsumer(nozzStateProcessor);
             wgt.addVisListener(visListener);
             boolean test = true;
             while(test){
@@ -57,5 +113,4 @@ public class TestWgtDeviceSdk {
             e.printStackTrace();
         }
     }
-
 }
